1a9a51e7bfb1ce175470738f26b7e8c70f1ef35b
Natfalluvia <amena_rny@outlook.com>
2023-05-13 12:50:11 +0800

    Temporarily remove the version

--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -58,7 +58,7 @@ public class CarpetSettings
      *     version name starts with "RNY"
      * </p>
      */
-    public static final String carpetVersion = "RNY-build-1-undefined";
+    public static final String carpetVersion = "RNY-current-undefined";
     public static final String minecraftVersion = "1.12.2";
     public static final String mcpMappings = "39-1.12";
 
