1ddc275c24c3095bbd7074d97b2890054877f9e5
Natfalluvia <amena_rny@outlook.com>
2023-05-17 16:43:57 +0800

    Test particles at path source

--- ChangeLog.md
+++ ChangeLog.md
@@ -8,3 +8,4 @@ Carpet 1.12 with [RNY](https://github.com/Rainyaphthyl)'s Addition
     - for PigZombie debugs
     - carpet.logging.logHelpers.PathLogHelper
     - net.minecraft.pathfinding.PathFinder::findPath
+    - PathNavigate line 302: Separate the path
--- carpetmodSrc/carpet/logging/logHelpers/PathReporter.java
+++ carpetmodSrc/carpet/logging/logHelpers/PathReporter.java
@@ -3,11 +3,14 @@ package carpet.logging.logHelpers;
 import carpet.logging.Logger;
 import carpet.logging.LoggerRegistry;
 import carpet.utils.Messenger;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.pathfinding.Path;
 import net.minecraft.pathfinding.PathPoint;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.WorldServer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +20,17 @@ public class PathReporter {
     public static final String DEFAULT_OPTION = LOGGER_OPTIONS[0];
     public static final String NAME = "pathFinding";
     private static Logger instance = null;
+    //private static IParticleData lvl1;
+    //private static IParticleData lvl2;
+    //private static IParticleData lvl3;
+    //
+    //static {
+    //    failedPath = parseParticle("angry_villager");
+    //    successfulPath = parseParticle("happy_villager");
+    //    lvl1 = parseParticle("dust 1 1 0 1");
+    //    lvl2 = parseParticle("dust 1 0.5 0 1");
+    //    lvl3 = parseParticle("dust 1 0 0 1");
+    //}
 
     public static Logger get_instance() {
         if (instance == null) {
@@ -25,7 +39,7 @@ public class PathReporter {
         return instance;
     }
 
-    public static void report(Path path) {
+    public static void report(Entity entity, Vec3d target, float milliseconds, Path path) {
         get_instance().log((option, player) -> {
             boolean visual = false, chat = false;
             switch (option) {
@@ -52,12 +66,46 @@ public class PathReporter {
                 }
             }
             if (visual) {
-                // TODO: 2023/5/17,0017 Visualize mob AI path
+                if (player instanceof EntityPlayerMP) {
+                    drawParticleLine((EntityPlayerMP) player, entity.getPositionVector(), target, 50, path != null);
+                }
             }
             return list.toArray(new ITextComponent[0]);
         });
     }
 
     public static void drawParticleLine(EntityPlayerMP player, Vec3d src, Vec3d dst, float ratio, boolean successful) {
+        if (player == null) {
+            return;
+        }
+        EnumParticleTypes accent = successful ? EnumParticleTypes.VILLAGER_HAPPY : EnumParticleTypes.VILLAGER_ANGRY;
+        ((WorldServer) player.world).spawnParticle(player, accent, true, src.x, src.y, src.z,
+                5, 0.5, 0.5, 0.5, 0.0);
+        //IParticleData accent = successful ? successfulPath : failedPath;
+        //IParticleData color = (ratio < 2)? lvl1 : ((ratio < 4)?lvl2:lvl3);
+        //
+        //((WorldServer)player.world).spawnParticle(
+        //        player,
+        //        accent,
+        //        true,
+        //        from.x, from.y, from.z, 5,
+        //        0.5, 0.5, 0.5, 0.0);
+        //
+        //double lineLengthSq = from.squareDistanceTo(to);
+        //if (lineLengthSq == 0) return;
+        //
+        //Vec3d incvec = to.subtract(from).normalize();//    multiply(50/sqrt(lineLengthSq));
+        //int pcount = 0;
+        //for (Vec3d delta = new Vec3d(0.0,0.0,0.0);
+        //     delta.lengthSquared()<lineLengthSq;
+        //     delta = delta.add(incvec.scale(player.world.rand.nextFloat())))
+        //{
+        //    ((WorldServer)player.world).spawnParticle(
+        //            player,
+        //            color,
+        //            true,
+        //            delta.x+from.x, delta.y+from.y, delta.z+from.z, 1,
+        //            0.0, 0.0, 0.0, 0.0);
+        //}
     }
 }
