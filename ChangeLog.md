9322f9acb8ef2f54d4f2d01011aff8c9d74d19c0
Natfalluvia <amena_rny@outlook.com>
2023-05-24 14:15:55 +0800

    Bump version to `RNY-build-3-20230524`

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,6 +1,6 @@
 Carpet 1.12 with [RNY](https://github.com/Rainyaphthyl)'s Addition
 
-[version](src/carpet/CarpetSettings.java): `RNY-build-3-********`
+[version](src/carpet/CarpetSettings.java): `RNY-build-3-20230524`
 
 # TO-DO List
 
@@ -8,6 +8,7 @@ Carpet 1.12 with [RNY](https://github.com/Rainyaphthyl)'s Addition
 - Add field `NAME` for all the logHelpers.
 - Display the actual option when setting logger with default options.
     - `Subscribed to <logger> \(<option>\)`
+- Bump to release versions after merging pathfinding-logger?
 
 # Feature Modifications
 
--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -34,7 +34,7 @@ public class CarpetSettings
 {
     public static boolean locked = false;
 
-    // Previous build ordinal: 1 (RNY-build-1-20230512)
+    // Previous build ordinal: 2 (RNY-build-2-20230518)
     // TODO: replace these constants at build time
     /**
      * format of version in Carpet-Addition-Naftalluvia(RNY):
@@ -58,7 +58,7 @@ public class CarpetSettings
      *     version name starts with "RNY"
      * </p>
      */
-    public static final String carpetVersion = "RNY-build-2-20230518";
+    public static final String carpetVersion = "RNY-build-3-20230524";
     public static final String minecraftVersion = "1.12.2";
     public static final String mcpMappings = "39-1.12";
 
