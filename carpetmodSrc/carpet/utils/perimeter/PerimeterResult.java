f1b78a327975f3bfba2df6e98ff71b12d9da0741
Natfalluvia <amena_rny@outlook.com>
2023-06-12 13:56:35 +0800

    Implement "updating" interfaces of PerimeterResult

--- carpetmodSrc/carpet/utils/perimeter/PerimeterResult.java
+++ carpetmodSrc/carpet/utils/perimeter/PerimeterResult.java
@@ -1,26 +1,24 @@
 package carpet.utils.perimeter;
 
+import it.unimi.dsi.fastutil.longs.LongAVLTreeSet;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import it.unimi.dsi.fastutil.objects.Object2IntAVLTreeMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-import it.unimi.dsi.fastutil.objects.Object2LongMap;
-import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2IntSortedMap;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nonnull;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import javax.annotation.Nullable;
+import java.util.*;
 
 public class PerimeterResult {
     private final Object2IntMap<EnumCreatureType> creatureCounts = new Object2IntOpenHashMap<>();
-    /**
-     * Spots are counted with long value: the higher bits for the spots outside 128m, the lower bits for the inner spots
-     */
-    private final Object2LongMap<EntityLiving.SpawnPlacementType> placementCounts = new Object2LongOpenHashMap<>();
-    private final Object2IntMap<Class<? extends EntityLiving>> specificCounts = new Object2IntOpenHashMap<>();
-    private final Map<Class<? extends EntityLiving>, Set<BlockPos>> spotSampleSets = new HashMap<>();
+    private final Map<EntityLiving.SpawnPlacementType, Object2IntSortedMap<EnumDistLevel>> placementCounts = new HashMap<>();
+    private final Map<Class<? extends EntityLiving>, Object2IntSortedMap<EnumDistLevel>> specificCounts = new HashMap<>();
+    private final Map<Class<? extends EntityLiving>, Map<EnumDistLevel, LongSet>> spotSampleSets = new HashMap<>();
 
     private PerimeterResult() {
     }
@@ -34,33 +32,75 @@ public class PerimeterResult {
         return result;
     }
 
-    public void addGeneralSpot(EntityLiving.SpawnPlacementType placementType, EnumDistLevel distLevel) {
-
+    @Nonnull
+    public static Object2IntSortedMap<EnumDistLevel> createNewLevelMap() {
+        Object2IntSortedMap<EnumDistLevel> tempMap = new Object2IntAVLTreeMap<>();
+        tempMap.defaultReturnValue(0);
+        return tempMap;
     }
 
-    public void addSpecificSample(BlockPos blockPos, EnumDistLevel distLevel) {
+    @Nonnull
+    public LongSet getSampleSet(Class<? extends EntityLiving> entityType, EnumDistLevel distLevel) {
+        Map<EnumDistLevel, LongSet> levelMap = spotSampleSets.computeIfAbsent(entityType, et -> new EnumMap<>(EnumDistLevel.class));
+        return levelMap.computeIfAbsent(distLevel, dl -> new LongAVLTreeSet());
+    }
 
+    @Nullable
+    public LongSet querySampleSet(Class<? extends EntityLiving> entityType, EnumDistLevel distLevel) {
+        LongSet tempSet = null;
+        Map<EnumDistLevel, LongSet> levelMap = spotSampleSets.get(entityType);
+        if (levelMap != null) {
+            tempSet = levelMap.get(distLevel);
+        }
+        return tempSet;
+    }
+
+    public void addGeneralSpot(EntityLiving.SpawnPlacementType placementType, EnumDistLevel distLevel) {
+        Object2IntSortedMap<EnumDistLevel> levelMap = placementCounts.computeIfAbsent(placementType, pt -> createNewLevelMap());
+        int count = levelMap.getInt(distLevel);
+        levelMap.put(distLevel, count + 1);
+    }
+
+    public void addSpecificSpot(Class<? extends EntityLiving> entityType, EnumDistLevel distLevel, BlockPos pos) {
+        boolean adding = false;
+        if (pos == null) {
+            adding = true;
+        } else {
+            long index = pos.toLong();
+            LongSet sampleSet = getSampleSet(entityType, distLevel);
+            if (!sampleSet.contains(index)) {
+                sampleSet.add(index);
+                adding = true;
+            }
+        }
+        if (adding) {
+            Object2IntSortedMap<EnumDistLevel> levelMap = specificCounts.computeIfAbsent(entityType, et -> createNewLevelMap());
+            int count = levelMap.getInt(distLevel);
+            levelMap.put(distLevel, count + 1);
+        }
     }
 
     public boolean containsPos(Class<? extends EntityLiving> entityType, BlockPos pos) {
         if (pos == null) {
             return false;
         }
-        boolean answer = false;
+        long index = pos.toLong();
+        List<Map<EnumDistLevel, LongSet>> maps = new ArrayList<>();
         if (entityType != null) {
-            Set<BlockPos> samples = spotSampleSets.get(entityType);
-            if (samples != null && samples.contains(pos)) {
-                answer = true;
-            }
+            maps.add(spotSampleSets.get(entityType));
         } else {
-            for (Set<BlockPos> samples : spotSampleSets.values()) {
-                if (samples != null && samples.contains(pos)) {
-                    answer = true;
-                    break;
+            maps.addAll(spotSampleSets.values());
+        }
+        for (Map<EnumDistLevel, LongSet> levelMap : maps) {
+            if (levelMap != null) {
+                for (LongSet samples : levelMap.values()) {
+                    if (samples != null && samples.contains(index)) {
+                        return true;
+                    }
                 }
             }
         }
-        return answer;
+        return false;
     }
 
     public enum EnumDistLevel {
